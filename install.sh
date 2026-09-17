33233aac3f9166184e6448fe87a707ca0d189af9
nil2509 <laishramNilkamal25@gmail.com>
2026-09-17 13:54:39 +0530

    Fixes

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-source "$SCRIPT_DIR/common.sh"
+source "$SCRIPT_DIR/scripts/common.sh"
 
 # ------------------------------------------------------------
 # Logging
